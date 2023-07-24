39f2c939223e92cbaf55ee8d10e5ed5800c6e48a
João Leahy <86070920+joaoleahy@users.noreply.github.com>
2023-07-24 09:15:27 -0500

    chore(lista_animais_screen): ajusta o o método do navigator para push

--- pet_connect/lib/screens/lista_animais_screen.dart
+++ pet_connect/lib/screens/lista_animais_screen.dart
@@ -15,6 +15,23 @@ class ListaAnimaisScreen extends StatelessWidget {
       'nome': 'Cachorro Caramelo',
       'foto': 'images/cachorro-caramelo.jpg',
     },
+    {import 'package:flutter/material.dart';
+import 'agendamento_screen.dart';
+
+class ListaAnimaisScreen extends StatelessWidget {
+  final List<Map<String, String>> animais = [
+    {
+      'nome': 'Gato Tricolor',
+      'foto': 'images/gato-tricolor.jpg',
+    },
+    {
+      'nome': 'Gato Cinza',
+      'foto': 'images/gato-cinza.jpg',
+    },
+    {
+      'nome': 'Cachorro Caramelo',
+      'foto': 'images/cachorro-caramelo.jpg',
+    },
     {
       'nome': 'Cachorro Preto',
       'foto': 'images/vira-lata-filhote-1.png',
@@ -43,7 +60,49 @@ class ListaAnimaisScreen extends StatelessWidget {
             ),
             title: Text(animais[index]['nome']!),
             onTap: () {
-              Navigator.pushReplacement(
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => AgendamentoScreen(
+                      animal: animais[index], entrevistadores: entrevistadores),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
+
+      'nome': 'Cachorro Preto',
+      'foto': 'images/vira-lata-filhote-1.png',
+    },
+  ];
+
+  final List<String> entrevistadores = [
+    'João da Silva',
+    'Maria Oliveira',
+    'Pedro Santos',
+    'Ana Souza',
+  ];
+
+  ListaAnimaisScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('PetConnect')),
+      body: ListView.builder(
+        itemCount: animais.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(animais[index]['foto']!),
+            ),
+            title: Text(animais[index]['nome']!),
+            onTap: () {
+              Navigator.push(
                 context,
                 MaterialPageRoute(
                   builder: (context) => AgendamentoScreen(
